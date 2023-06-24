8f0777adc2ec3e86030eec9d083749eef431704b
devCadet <satyam.karn3629@gmail.com>
2023-06-24 23:31:13 +0530

    updated script

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.getElementById("dateTime").innerHTML = d.toDateString()
 
 // API integration
 
-const API_URL  = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const API_KEY  = "adc68f9a746902618f465bc19a3cd475";
+const apiKey = "adc68f9a746902618f465bc19a3cd475";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 
 // 
@@ -14,7 +14,9 @@ const searchBtn = document.querySelector('.submit')
 const searchBox = document.querySelector('.input')
 
 async function checkWeather(city) {
-    const response = await fetch(API_URL + city + `&appid=${API_KEY}`)
+    const response = await fetch(apiUrl + city +`&appid=${apiKey}`)
+
+
     var data = await response.json();
     console.log(data);
 
